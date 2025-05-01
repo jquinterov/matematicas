3dce736c52926dd6889078b00ef6fe055602ac83
Jhonatan Quintero <jhonatan.quintero@lineadatascan.com>
2025-05-01 12:42:37 -0500

    Otras funciones

--- calculadora.js
+++ calculadora.js
@@ -5,3 +5,14 @@ function sumar(a, b) {
   function restar(a, b) {
     return a - b;
   }
+  
+  function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  function dividir(a, b) {    
+    if (b === 0) {
+      return "No se puede dividir entre 0";
+    }
+    return a / b;
+  }
