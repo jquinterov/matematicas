5539c65fed497b9cd44bac7a13af2d69765ae8c1
Jhonatan Quintero <jhonatan.quintero@lineadatascan.com>
2025-05-01 14:08:25 -0500

    agregadas nuevas funciones matematicas

--- calculadora.js
+++ calculadora.js
@@ -16,3 +16,37 @@ function sumar(a, b) {
     }
     return a / b;
   }
+  
+  function potencia(a, b) {
+    return Math.pow(a, b);
+  }
+  
+  function raiz(a, b) {
+    return Math.pow(a, 1/b);
+  }
+  
+  function logaritmo(a, b) {
+    return Math.log(a) / Math.log(b);
+  }
+  
+  function seno(a) {
+    return Math.sin(a);
+  }
+  
+  function coseno(a) {
+    return Math.cos(a);
+  }
+  
+  function tangente(a) {
+    return Math.tan(a);
+  }
+  
+  function factorial(a) {
+    if (a < 0) {
+      return "No se puede calcular el factorial de un número negativo";
+    }
+    if (a === 0) {
+      return 1;
+    }
+    return a * factorial(a - 1);
+    }
